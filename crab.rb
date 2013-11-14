6aade6ddc5ddde8c4803ada2ee7847efeef7c575
v3rtx <den.groove@gmail.com>
2013-11-14 16:49:49 +0300

    crab ver 1.0

--- crab.rb
+++ crab.rb
@@ -1,30 +1,27 @@
 #!/usr/bin/env ruby
+WIDTH = 20
+
 
 file = File.open("crab");
 crab_arr = file.to_a
 crab_rev = []
-crab_arr.each{ |item| crab_rev << ' '*10 + item.chomp.reverse! }
+crab_arr.each{ |item| crab_rev << ' '*WIDTH + item.chomp.reverse! }
 puts crab_rev
 crab_temp = Array.new(crab_arr)
 
 direction = true
 (0..1000).each do |i|
   puts crab_temp
-  sleep 0.1
+  sleep 0.05
   if direction == true
-    crab_temp.each{ |item| item.insert(0,' ') }
+    crab_temp = crab_arr.map{ |item| ' '*(i%WIDTH)+item }
   else
-    crab_temp.each{ |item| item.slice!(0) }    
+    crab_temp = crab_rev.map{ |item| item[i%WIDTH..item.length-1] }    
   end 
   
-  if (i % 10 == 9) then
+  if (i % WIDTH == WIDTH-1) then
     direction = !direction
     crab_temp.clear
-    puts '='*40
-    puts crab_arr
-    puts '='*40
-    puts crab_rev
-    puts '='*40
     if direction == true then
       crab_temp = Array.new(crab_arr)
     else

980bcbe320ff78d2c9edee28c18a63539e6522f3
v3rtx <den.groove@gmail.com>
2013-11-14 10:19:55 +0300

    crab ver 0.9

--- crab.rb
+++ crab.rb
@@ -2,30 +2,34 @@
 
 file = File.open("crab");
 crab_arr = file.to_a
-crab_temp = Array.new()
-puts crab_arr
-crab_arr.each{ |item, index| 
-	puts index }
-	#crab_arr[index] = item.chomp.reverse! }
-puts crab_arr
+crab_rev = []
+crab_arr.each{ |item| crab_rev << ' '*10 + item.chomp.reverse! }
+puts crab_rev
+crab_temp = Array.new(crab_arr)
 
-#flag = true
-#(0..1000).each do |i|
-#  puts crab_temp
-#  sleep 0.1
-#  if flag == true
-#    crab_temp.each{ |item| item.insert(0,' ') }
-#  else
-#    crab_temp.each{ |item| item = item.slice(0) }    
-#  end 
-#  
-#  if (i % 10 == 9) then
-#    crab_temp = []  
-#    crab_arr.each{ |item| crab_temp << item.chomp.reverse! }    
-#    puts crab_arr
-#    crab_temp.each{ |item| item.insert(0, ' '*10) }
-#    puts crab_temp
-#    flag = !flag
-#  end  
-#end
+direction = true
+(0..1000).each do |i|
+  puts crab_temp
+  sleep 0.1
+  if direction == true
+    crab_temp.each{ |item| item.insert(0,' ') }
+  else
+    crab_temp.each{ |item| item.slice!(0) }    
+  end 
+  
+  if (i % 10 == 9) then
+    direction = !direction
+    crab_temp.clear
+    puts '='*40
+    puts crab_arr
+    puts '='*40
+    puts crab_rev
+    puts '='*40
+    if direction == true then
+      crab_temp = Array.new(crab_arr)
+    else
+      crab_temp = Array.new(crab_rev.dup)
+    end
+  end  
+end
 
